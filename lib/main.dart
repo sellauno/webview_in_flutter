a37f577bdc7fceacf8d4640ea8c8e75a37e2e801
sellauno <sellauno03@gmail.com>
2022-09-19 22:26:51 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'dart:io'; 
+import 'package:flutter/material.dart';
+// import 'package:webview_flutter/webview_flutter.dart';
+import 'src/web_view_stack.dart'; 
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: WebViewApp(),
+    ),
+  );
+}
+
+class WebViewApp extends StatefulWidget {
+  const WebViewApp({Key? key}) : super(key: key);
+
+  @override
+  State<WebViewApp> createState() => _WebViewAppState();
+}
+
+class _WebViewAppState extends State<WebViewApp> {
+
+  @override
+  // void initState() {
+  //   if (Platform.isAndroid) {
+  //     WebView.platform = SurfaceAndroidWebView();
+  //   }
+  //   super.initState();
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter WebView'),
+      ),
+      // body: const WebView(
+      //   initialUrl: 'https://flutter.dev',
+      // ),
+      
+      body: const WebViewStack(),
+    );
+  }
+}
